e08ef4944a03f78d581c213784e14b9cc2cf523f
Christopher Spagnesi <christopherspagnesi@gmail.com>
2020-12-02 13:58:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,10 @@
 def current_player(board)
   turn_count(board) % 2 == 0? "X" : "O"
 end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
